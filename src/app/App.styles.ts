a78883558041563736977a4ba40a4f8f15213f02
Константин Безпалов <KKBezpalov@sberbank.ru>
2021-12-04 16:31:03 +0300

    add prettier eslint

--- src/app/App.styles.ts
+++ src/app/App.styles.ts
@@ -1,5 +1,5 @@
 import styled from '@emotion/styled';
 
 export default {
-    Title: styled.h1({color: 'red'})
-}
+    Title: styled.h1({color: 'red'}),
+};
--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,7 +2,8 @@ import Styled from './App.styles';
 
 export const App: React.FC = () => {
     return (
-    <>
-        <Styled.Title>{process.env.STAND}</Styled.Title>
-    </>)
-}
+        <>
+            <Styled.Title>{324234234}</Styled.Title>
+        </>
+    );
+};
