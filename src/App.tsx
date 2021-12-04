d3ab055cd315abed7e7128c0d12cb5fc56e80a66
Константин Безпалов <KKBezpalov@sberbank.ru>
2021-12-04 15:04:45 +0300

    config webpack

--- src/App.tsx
+++
@@ -1,5 +0,0 @@
-import Styled from './App.styles'
-
-export const App: React.FC = () => {
-    return <Styled.Title>TEXT</Styled.Title>
-}
---
+++ src/app/App.tsx
@@ -0,0 +1,8 @@
+import Styled from './App.styles';
+
+export const App: React.FC = () => {
+    return (
+    <>
+        <Styled.Title>{process.env.STAND}</Styled.Title>
+    </>)
+}
