fca146cf94ebb4d58e12d73090ba8bd4fd8874f7
Константин Безпалов <KKBezpalov@sberbank.ru>
2021-12-05 13:02:14 +0300

    add speed plagin for webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,8 +1,11 @@
+const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 const {merge} = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
 const common = require('./webpack.common.js');
 
-module.exports = ({env}) => {
+const smp = new SpeedMeasurePlugin();
+
+module.exports = smp.wrap(({env}) => {
     const configName = env === 'storybook' ? 'dev' : env;
     const config = require(`./webpack.${configName}.js`);
 
@@ -13,4 +16,4 @@ module.exports = ({env}) => {
     );
 
     return merge(common, config);
-};
+});
